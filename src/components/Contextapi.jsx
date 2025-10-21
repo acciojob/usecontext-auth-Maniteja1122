1efdbe7a05539cd6a6a771d90632fa137e821d5f
Maniteja1122 <kuntamaniteja0607@gmail.com>
2025-10-22 00:30:25 +0530

    updated context

--- src/components/Contextapi.jsx
+++ src/components/Contextapi.jsx
@@ -3,16 +3,22 @@ import { authContext } from "../context/AuthProvider";
 
 const Contextapi = () => {
   const { isHuman, setIsHuman } = useContext(authContext);
+
   return (
     <div>
-      <h1>click on the checkbox to get authenticated</h1>
-      {isHuman
-        ? "you are now authenticated, you can proceed"
-        : "you are not authenticated"}
-        <br/>
+      <h1>Click on the checkbox to get authenticated</h1>
+      
+      <p className="authText">
+        {isHuman
+          ? "you are now authenticated, you can proceed"
+          : "you are not authenticated"}
+      </p>
+
+      <br />
+
       <input
         type="checkbox"
-        value={isHuman}
+        checked={isHuman}
         onChange={(e) => setIsHuman(e.target.checked)}
       />
       <label>I'm not a robot</label>
